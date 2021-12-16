85a25b4adc4c1b112c0712a0520e326f36e15ab4
anthony <tosharus26@gmail.com>
2021-12-17 01:34:37 +0300

    debag

--- keyboards/default/keyboards.py
+++ keyboards/default/keyboards.py
@@ -5,7 +5,5 @@ button_subscribe = KeyboardButton('♡ Subscribe')
 button_info = KeyboardButton('Help !')
 button_history = KeyboardButton('📖 Histiry')
 
-
-
 greed_kb = ReplyKeyboardMarkup(resize_keyboard=True)\
     .add(button_buy,button_subscribe).add(button_info,button_history)
