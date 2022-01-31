bedd1664e2a3282d70f9fd7292a628c363fdacae
anthony <tosharus26@gmail.com>
2022-02-01 00:23:45 +0300

    работают кнопки процессора

--- handlers/menu/menu_callback.py
+++ handlers/menu/menu_callback.py
@@ -7,12 +7,13 @@ from keyboards.inline.inline_cpu import cpu_mane
 @dp.callback_query_handler(text="CPU")
 async def button_store(callback: types.CallbackQuery):
     await callback.message.answer(text="🛒 Select CPU", reply_markup=cpu_mane.cpu)
+    await callback.answer()
 
 
 @dp.callback_query_handler(text="GPU")
 async def button_store(callback: types.CallbackQuery):
     await callback.message.answer(text="🛒 Select GPU", reply_markup=gpu_mane.gpu)
-
+    await callback.answer()
 #
 # @dp.callback_query_handler(text="RAM")
 # async def button_store(callback: types.CallbackQuery):
