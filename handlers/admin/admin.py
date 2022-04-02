5a9b3808bb586eb0843c797e79d69170520c8990
Anthony <tosharus26@gmail.com>
2022-04-02 20:44:52 +0300

    Вроде с админкой всё. Дальше прикручивание базы

--- handlers/admin/admin.py
+++ handlers/admin/admin.py
@@ -1,10 +1,11 @@
 from aiogram import types, Bot, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from loader import dp
+from loader import dp, bot
 from data import config
-from aiogram.dispatcher.filters import  Text
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+from aiogram.dispatcher.filters import Text
+
+ID = None
 
 
 # Админка бота
@@ -15,43 +16,56 @@ class Admin_bot(StatesGroup):
     price = State()
 
 
+@dp.message_handler(commands="moderator", is_chat_admin=True)
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, "Введи команду")
+    await message.delete()
+
+
 @dp.message_handler(commands="admin_panel", states=None)
 async def cm_start(message: types.Message):
-    await Admin_bot.name.set()
-    await message.reply('Введите название продукта')
+    if message.from_user.id == ID:
+        await Admin_bot.name.set()
+        await message.reply('Введите название продукта')
 
 
 @dp.message_handler(state='*', commands='отмена')
 @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
-async def cancel_handler(messege: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await messege.reply('Ok')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('Ok')
 
 
 @dp.message_handler(state=Admin_bot.name)
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    await Admin_bot.next()
-    await message.reply('Введите описание')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await Admin_bot.next()
+        await message.reply('Введите описание')
 
 
 @dp.message_handler(state=Admin_bot.discription)
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['discription'] = message.text
-    await Admin_bot.next()
-    await message.reply('Введите цену')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['discription'] = message.text
+        await Admin_bot.next()
+        await message.reply('Введите цену')
 
 
 @dp.message_handler(state=Admin_bot.price)
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = message.text
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = message.text
 
-    async with state.proxy() as data:
-        await message.reply(str(data))
-    await state.finish()
+        async with state.proxy() as data:
+            await message.reply(str(data))
+        await state.finish()
