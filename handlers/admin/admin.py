737f4618dec17d9e6087b41caa3a90e4fee01ff6
Anthony <tosharus26@gmail.com>
2022-04-02 20:03:27 +0300

    Админка работает

--- handlers/admin/admin.py
+++ handlers/admin/admin.py
@@ -3,20 +3,32 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from loader import dp
 from data import config
-
+from aiogram.dispatcher.filters import  Text
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+
+
 # Админка бота
 
 class Admin_bot(StatesGroup):
     name = State()
-    #    discription = State()
+    discription = State()
     price = State()
 
 
-@dp.message_handler(commands="Загрузить", states=None)
+@dp.message_handler(commands="admin_panel", states=None)
 async def cm_start(message: types.Message):
     await Admin_bot.name.set()
-    await message.reply('Напиши название')
+    await message.reply('Введите название продукта')
+
+
+@dp.message_handler(state='*', commands='отмена')
+@dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
+async def cancel_handler(messege: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await messege.reply('Ok')
 
 
 @dp.message_handler(state=Admin_bot.name)
@@ -24,22 +36,22 @@ async def load_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
     await Admin_bot.next()
-    await message.reply('Введите название')
+    await message.reply('Введите описание')
+
+
+@dp.message_handler(state=Admin_bot.discription)
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['discription'] = message.text
+    await Admin_bot.next()
+    await message.reply('Введите цену')
 
 
 @dp.message_handler(state=Admin_bot.price)
 async def load_price(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['price'] = message.text
-    await Admin_bot.next()
-    await message.reply('Введите цену')
 
     async with state.proxy() as data:
         await message.reply(str(data))
     await state.finish()
-
-# def register_habdlers_admin(dp: Dispatcher):
-#     dp.register_message_handler(button_info, text="text")
-#     dp.register_message_handler(cm_start, commands="Загрузить", states=None)
-#     dp.register_message_handler(load_name, state=Admin_bot.name)
-#     dp.register_message_handler(load_price, state=Admin_bot.price)
