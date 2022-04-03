ffdaf0daabdc9a01c41e90a5b457306bde79f38b
anthony <tosharus26@gmail.com>
2022-04-03 22:32:21 +0300

    Предположение того как должен выглядеть запрос по созданию бд. Сейчас буду создавать функцию

--- data_base/sql_admin.py
+++ data_base/sql_admin.py
@@ -8,16 +8,19 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('Data base connected OK!')
-    base.execute('CREATE TABLE IF NOT EXISTS menu(name TEXT PRIMARY KEY,discription TEXT, price TEXT)')
+    base.execute('CREATE TABLE IF NOT EXISTS store_menu(item_id INTEGER PRIMARY KEY,name TEXT, '
+                 'discription TEXT, price INTEGER, amount INTEGER, img TEXT)')
     base.commit()
 
 
 async def sql_add_command(state):
     async with state.proxy() as data:
-        cur.execute('INSERT INTO menu VALUES (?, ?, ?)', tuple(data.values()))
+        cur.execute('INSERT INTO menu VALUES (?,?,?,?,?,?)', tuple(data.values()))
         base.commit()
 
 
 async def sql_read(message):
     for ret in cur.execute('SELECT * FROM menu').fetchall():
-        await bot.send_message(message.from_user.id, f'Название: {ret[0]}\nОписание: {ret[1]}\nЦена: {ret[2]}')
+        await bot.send_message(message.from_user.id, f'ID товара: {ret[0]}\nНазвание товара: {ret[1]}\n'
+                    f'Описание: {ret[2]}\nЦена: {ret[3]}$\nКолличество в наличии: {ret[4]}'
+                                                     f'{ret[5]}')
