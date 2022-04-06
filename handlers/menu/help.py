4a5f1001e00c8b4a5c50b142f3a47aedcd420933
anthony <tosharus26@gmail.com>
2022-04-07 02:53:03 +0300

    Сделал проверку на админа из конфига

--- handlers/menu/help.py
+++ handlers/menu/help.py
@@ -6,8 +6,9 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
+    await message.delete()
     text = ("Список команд: ",
             "/start - Начать диалог",
             "/help - Получить справку")
-    
+
     await message.answer("\n".join(text))
