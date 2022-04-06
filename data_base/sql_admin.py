dc7e02515e62371c39afdfd724f41a9d1143d29c
anthony <tosharus26@gmail.com>
2022-04-06 12:41:23 +0300

    Сделал таблицу check_admin

--- data_base/sql_admin.py
+++ data_base/sql_admin.py
@@ -8,8 +8,11 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('Data base connected OK!')
+    # SQL запрос на создание таблицы 'store_memu'
     base.execute('CREATE TABLE IF NOT EXISTS store_menu(item_id INTEGER PRIMARY KEY,name TEXT, '
                  'discription TEXT, price INTEGER, amount INTEGER, img_item TEXT)')
+    # SQL запрос на создание таблицы 'check_admin'
+    base.execute('CREATE TABLE IF NOT EXISTS check_admin(admin_id INTEGER PRIMARY KEY, name TEXT)')
     base.commit()
 
 
@@ -19,7 +22,13 @@ async def sql_add_command(state):
         base.commit()
 
 
+# async def sql_read(message):
+#     for ret in cur.execute('SELECT * FROM store_menu').fetchall():
+#         await bot.send_photo(message.from_user.id, ret[5], f'ID товара: {ret[0]}\nНазвание товара: {ret[1]}\n'
+#                     f'Описание: {ret[2]}\nЦена: {ret[3]}$\nКолличество в наличии: {ret[4]}\n')
+
+
 async def sql_read(message):
     for ret in cur.execute('SELECT * FROM store_menu').fetchall():
-        await bot.send_photo(message.from_user.id, ret[5], f'ID товара: {ret[0]}\nНазвание товара: {ret[1]}\n'
-                    f'Описание: {ret[2]}\nЦена: {ret[3]}$\nКолличество в наличии: {ret[4]}\n')
+        await bot.send_photo(message.from_user.id, ret[5], f'Название товара: {ret[1]}\n'
+                                                           f'Описание: {ret[2]}\nЦена: {ret[3]} ₽\n')
