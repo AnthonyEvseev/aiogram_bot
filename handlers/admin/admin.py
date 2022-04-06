6722af3785467b66df020c4fe3bdafe717d95791
anthony <tosharus26@gmail.com>
2022-04-06 23:01:06 +0300

    Сделал проверку на админа из конфига

--- handlers/admin/admin.py
+++ handlers/admin/admin.py
@@ -1,10 +1,7 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types
 from loader import dp, bot
-from aiogram.dispatcher import FSMContext
-from data_base import sql_admin
-
-ID = None
+from configs.config import ADMINS
 
 
 # Админка бота
@@ -19,19 +16,11 @@ class Admin_bot(StatesGroup):
     img_item = State()
 
 
-@dp.message_handler(commands='admin')
-async def check_admin(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['check_admin'] = message.from_user.id
-    await message.reply('Проверка админа пройдена')
-    await sql_admin.sql_check_admin(state)
-    await state.finish()
-
-
-
-@dp.message_handler(commands="mod", is_chat_admin=True)
+@dp.message_handler(commands="mod")
 async def make_changes_command(message: types.Message):
-    global ID
-    ID = message.from_user.id
-    await bot.send_message(message.from_user.id, "Введи команду")
+    for admin in ADMINS:
+        try:
+            await bot.send_message(message.from_user.id, "Введи команду")
+        except:
+            pass
     await message.delete()
