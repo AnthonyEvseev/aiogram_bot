c559e4e5a99e426ebb72f41caf0e8054b9dd650d
anthony <tosharus26@gmail.com>
2022-04-07 20:47:59 +0300

    rename callback

--- handlers/menu/start.py
+++ handlers/menu/start.py
@@ -1,10 +1,10 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-from keyboards.keyboards.keyboards import greed_kb
+from keyboards.keyboards.keyboards_mane import mane_menu
 
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!", reply_markup=greed_kb)
+    await message.answer(f"Привет, {message.from_user.full_name}!", reply_markup=mane_menu)
