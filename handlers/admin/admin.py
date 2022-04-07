7c506db020025d4a70943b5890aadeceaca508f3
anthony <tosharus26@gmail.com>
2022-04-07 20:47:47 +0300

    rename callback

--- handlers/admin/admin.py
+++ handlers/admin/admin.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from loader import dp, bot
 from configs.config import ADMINS
+from keyboards.keyboards.keyboards_admin import mane_admin
 
 
 # Админка бота
@@ -10,6 +11,6 @@ async def make_changes_command(message: types.Message):
     await message.delete()
     for admin in ADMINS:
         if message.from_user.id == int(admin):
-            await bot.send_message(message.chat.id, "Введи команду")
+            await bot.send_message(message.chat.id, "Введи команду", reply_markup=mane_admin)
         else:
             await bot.send_message(message.chat.id, "У Вас нет прав администратора")
