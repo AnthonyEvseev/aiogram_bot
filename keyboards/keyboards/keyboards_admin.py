f8b624253e523c9ac1af402d7b4e9476eca1d3c1
anthony <tosharus26@gmail.com>
2022-04-10 13:14:06 +0300

    rename button_append

--- keyboards/keyboards/keyboards_admin.py
+++ keyboards/keyboards/keyboards_admin.py
@@ -1,8 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-button_add = KeyboardButton('➕ Add')
+button_append = KeyboardButton('➕ Append')
 button_delete = KeyboardButton('🗑️ Delete')
 button_start = KeyboardButton('/start')
 
 mane_admin = ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="")
-mane_admin.add(button_add, button_delete).add(button_start)
+mane_admin.add(button_append, button_delete).add(button_start)
