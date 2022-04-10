9cb8e294403eed54a3c410e7c1a92e1bb90221d8
anthony <tosharus26@gmail.com>
2022-04-11 00:58:03 +0300

    Изменил меню и добавил хендлеров

--- handlers/menu/menu.py
+++ handlers/menu/menu.py
@@ -1,13 +1,11 @@
 from loader import dp, bot
 from aiogram import types
 from data_base import sql_admin
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from keyboards.inline_keyboards.menu_inline import menu
 
 
 @dp.message_handler(text="🍴 Menu")
 async def button_base(message: types.Message):
     read = await sql_admin.sql_read_store_menu()
     for ret in read:
-        await bot.send_photo(message.from_user.id, ret[5], reply_markup=InlineKeyboardMarkup().
-                             add(InlineKeyboardButton(f' - "{ret[1]}"',
-                                                      callback_data=f'delete {ret[1]}')))
+        await bot.send_photo(message.from_user.id, ret[5], f'{ret[1]}\n{ret[2]}\nЦена: {ret[3]}₽', reply_markup=menu)
--- keyboards/inline_keyboards/menu_inline.py
+++ keyboards/inline_keyboards/menu_inline.py
@@ -1,10 +1,19 @@
 from aiogram.types import InlineKeyboardMarkup
 
-cpu = InlineKeyboardMarkup(text='CPU',
-                           callback_data='CPU')
+prise = InlineKeyboardMarkup(text='Сумма: ₽',
+                           callback_data='none')
 
-gpu = InlineKeyboardMarkup(text='GPU',
-                           callback_data='GPU')
+minus = InlineKeyboardMarkup(text='➖',
+                             callback_data='➖')
 
-menu = InlineKeyboardMarkup(row_width=2)
-menu.add(cpu, gpu)
+none_item = InlineKeyboardMarkup(text='шт.',
+                                 callback_data='none')
+
+plus = InlineKeyboardMarkup(text='➕',
+                            callback_data='➕')
+
+buy = InlineKeyboardMarkup(text='Добавить в корзину',
+                           callback_data='buy')
+
+menu = InlineKeyboardMarkup(row_width=3)
+menu.add(prise).add(minus, none_item, plus).add(buy)
