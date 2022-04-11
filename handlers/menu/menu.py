ca4f83fd334dfa44f812b039e5e3d9d860bb8acc
anthony <tosharus26@gmail.com>
2022-04-11 19:04:45 +0300

    Незначительное переименнование

--- handlers/menu/menu.py
+++ handlers/menu/menu.py
@@ -6,7 +6,7 @@ from keyboards.inline_keyboards.menu_inline import menu
 
 @dp.message_handler(text="🍴 Menu")
 async def button_base(message: types.Message):
-    read = await sql_admin.sql_read_store_menu()
-    for ret in read:
+    read_db = await sql_admin.sql_read_store_menu()
+    for ret in read_db:
         await bot.send_photo(message.from_user.id, ret[5],
                              f'{ret[1]}\n{ret[2]}\nЦена: {ret[3]}₽', reply_markup=menu)
