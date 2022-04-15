33034fdf4bf77decc4622b1baf9b5b44df193b26
anthony <tosharus26@gmail.com>
2022-04-15 16:10:52 +0300

    удалил точку

--- handlers/admin/admin_delete.py
+++ handlers/admin/admin_delete.py
@@ -9,7 +9,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @dp.callback_query_handler(Text(startswith='delete '))
 async def callback_delete(callback: types.CallbackQuery):
     await sql_admin.sql_delete_item_store_menu(callback.data.replace('delete ', ''))
-    await callback.message.answer(text=f"{callback.data.replace('delete ', '')} удалена.")
+    await callback.message.answer(text=f"{callback.data.replace('delete ', '')} удалена")
     await callback.answer()
 
 
