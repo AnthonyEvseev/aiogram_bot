47c4111e936f96360efe8074b82704116d99c4e2
anthony <tosharus26@gmail.com>
2022-04-15 11:10:12 +0300

    Перенес кнопки

--- handlers/menu/menu.py
+++ handlers/menu/menu.py
@@ -10,3 +10,21 @@ async def button_base(message: types.Message):
     for ret in read_db:
         await bot.send_photo(message.from_user.id, ret[5],
                              f'{ret[1]}\n{ret[2]}\nЦена: {ret[3]}₽', reply_markup=menu)
+
+
+# Отлавливает нажатие на кнопку "🛒 Store"
+@dp.message_handler(text="🛒 Basket")
+async def button_store(message: types.Message):
+    await message.answer(text="🛒 Choose a product category!", reply_markup=menu)
+
+
+# Отлавливает нажатие на кнопку "❗ Info"
+@dp.message_handler(text="❗ Info")
+async def button_info(message: types.Message):
+    await bot.send_message(message.chat.id,
+                           "Hellow, Я TonyTestBot 🚀\n"
+                           "Для друзей просто Tony 😏\n\n"
+                           'После нажатия на кнопку "Store"\n'
+                           'откроется интернет магазин,\n'
+                           'где ты сможешь сделать заказ 🛒\n\n'
+                           "Я пока не закончен, но скоро всё будет 🔥")
