a0d6126728e4163c7115037d0bd31d8fcf29a237
anthony <tosharus26@gmail.com>
2022-04-15 11:11:39 +0300

    Почистил до дефолта

--- keyboards/delete_item_admin.py
+++ keyboards/delete_item_admin.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup
 
-delete = InlineKeyboardMarkup(text='delete ',
-                              callback_data='delete ')  # f'delete ret[1]
+delete = InlineKeyboardMarkup(text='Удалить',
+                              callback_data=f'delete ret[1]')
 
 delete_item = InlineKeyboardMarkup(row_width=3)
 delete_item.add(delete)
