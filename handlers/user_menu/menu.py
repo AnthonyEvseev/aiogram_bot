5a80cfb451d68e92531aef04fce1ec93def08825
anthony <tosharus26@gmail.com>
2022-04-19 19:41:13 +0300

    Доооооолго настраивал колбек. Вроде работает

--- handlers/user_menu/menu.py
+++ handlers/user_menu/menu.py
@@ -1,27 +1,39 @@
-
+from aiogram.utils.callback_data import CallbackData
 from loader import dp, bot
 from aiogram import types
 from data_base import sql_admin
+# from data_base.sql_admin import sql_read_store_menu_test
 
 
-def generate_kb(current_amount):
+@dp.message_handler(text="🍴 Menu")
+async def button_base(message: types.Message):
+    await sql_admin.sql_read_store_menu_test(message)
+
+# @dp.message_handler(text="🍴 Menu")
+# async def button_base(message: types.Message):
+#     read_db = await sql_admin.sql_read_store_menu()
+#     kb = generate_kb(0)
+#     for ret in read_db:
+#         await bot.send_photo(message.from_user.id, ret[4],
+#                              f'{ret[1]}\n{ret[2]}\nЦена: {ret[3]}₽', reply_markup=kb)
+
+callback_menu = CallbackData('item', 'item_id', 'amount')
+
+
+async def make_callback_menu(item_id='0', amount='0'):
+    return callback_menu.new(item_id=item_id, amount=amount)
+
+
+def generate_kb(current_amount, name, item):
     current_amount = int(current_amount)
-
+    # global callback_menu
     keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
     keyboard_markup.add(
         types.InlineKeyboardButton('-', callback_data='counter_{}'.format(current_amount - 1)),
         types.InlineKeyboardButton('{} шт'.format(current_amount), callback_data='empty?'),
         types.InlineKeyboardButton('+', callback_data='counter_{}'.format(current_amount + 1)),
-        types.InlineKeyboardButton('Добавить в корзину', callback_data='cart_add_{}'.format(current_amount)),
+        types.InlineKeyboardButton(f'Добавить в корзину: {name}', callback_data=f'cart_add_{item}'),
+        # types.InlineKeyboardButton('Добавить в корзину', callback_data='cart_add_{}'.format(current_amount)),
     )
 
     return keyboard_markup
-
-
-@dp.message_handler(text="🍴 Menu")
-async def button_base(message: types.Message):
-    read_db = await sql_admin.sql_read_store_menu()
-    kb = generate_kb(0)
-    for ret in read_db:
-        await bot.send_photo(message.from_user.id, ret[4],
-                             f'{ret[1]}\n{ret[2]}\nЦена: {ret[3]}₽', reply_markup=kb)
