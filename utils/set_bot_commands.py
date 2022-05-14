b483507cefd3d198ca5f87aa50babc64a8f5134f
anthony <tosharus26@gmail.com>
2022-05-14 04:01:55 +0300

    cleancode

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,6 +5,8 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Вывести справку")
+            types.BotCommand("help", "Вывести справку"),
+            types.BotCommand("mod", "Режим администратора"),
+            types.BotCommand("cansel", "Отмена ввода")
         ]
     )
